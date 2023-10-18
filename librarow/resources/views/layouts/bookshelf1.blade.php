1183c5f55d0e6ba492b8d8c6bda3c2919fe158a9
Rio Saputro <rio.ae23@gmail.com>
2023-10-19 00:06:41 +0700

    Homepage done
    adding unique title and give temporary layout

--- librarow/resources/views/layouts/bookshelf1.blade.php
+++ librarow/resources/views/layouts/bookshelf1.blade.php
@@ -6,7 +6,7 @@
         <div class="w-1/5 h-full ">
             <div class="flex flex-col text-center">
                 <img src="./img/drama.jpg" alt="" class="w-3/4 h-48 object-contain mx-auto mt-2 mb-5">
-                <h4 class="font-bold uppercase mb-3">Anjay</h4>
+                <h4 class="font-bold uppercase mb-3">Book</h4>
                 <button
                     class="bg-blue-500 text-white rounded-md mx-auto w-2/4 h-6 hover:text-2xl hover:scale-150 hover:bg-lime-200">Borrow</button>
             </div>
@@ -14,28 +14,28 @@
         <div class="w-1/5 h-full ">
             <div class="flex flex-col text-center">
                 <img src="./img/comic.jpg" alt="" class="w-3/4 h-48 object-contain mx-auto mt-2 mb-5">
-                <h4 class="font-bold uppercase mb-3">Anjay</h4>
+                <h4 class="font-bold uppercase mb-3">Book</h4>
                 <button class="bg-blue-500 text-white rounded-md mx-auto w-2/4 h-6">Borrow</button>
             </div>
         </div>
         <div class="w-1/5 h-full ">
             <div class="flex flex-col text-center">
                 <img src="./img/horror.jpg" alt="" class="w-3/4 h-48 object-contain mx-auto mt-2 mb-5">
-                <h4 class="font-bold uppercase mb-3">Anjay</h4>
+                <h4 class="font-bold uppercase mb-3">Book</h4>
                 <button class="bg-blue-500 text-white rounded-md mx-auto w-2/4 h-6">Borrow</button>
             </div>
         </div>
         <div class="w-1/5 h-full ">
             <div class="flex flex-col text-center">
                 <img src="./img/naruto.jpg" alt="" class="w-3/4 h-48 object-contain mx-auto mt-2 mb-5">
-                <h4 class="font-bold uppercase mb-3">Anjay</h4>
+                <h4 class="font-bold uppercase mb-3">Book</h4>
                 <button class="bg-blue-500 text-white rounded-md mx-auto w-2/4 h-6">Borrow</button>
             </div>
         </div>
         <div class="w-1/5 h-full">
             <div class="flex flex-col text-center">
                 <img src="./img/stevejob.png" alt="" class="w-3/4 h-48 object-contain mx-auto mt-2 mb-5">
-                <h4 class="font-bold uppercase mb-3">Anjay</h4>
+                <h4 class="font-bold uppercase mb-3">Book</h4>
                 <button class="bg-blue-500 text-white rounded-md mx-auto w-2/4 h-6">Borrow</button>
             </div>
         </div>
