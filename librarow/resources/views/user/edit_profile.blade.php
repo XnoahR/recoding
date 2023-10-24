d0efb3a97a56967f4c433a3e78a7ead09c931d12
Rio Saputro <rio.ae23@gmail.com>
2023-10-24 20:20:53 +0700

    Profile photo edit

--- librarow/resources/views/user/edit_profile.blade.php
+++ librarow/resources/views/user/edit_profile.blade.php
@@ -2,7 +2,9 @@
 
 @section('main')
 <section id="profile_edit" class="h-screen w-5/12  shadow-lg rounded-xl text-center mx-auto border border-blue-500 border-opacity-60 my-10">
-Tobirama!
+<h2 class="font-bold text-lg mt-3 mb-1">Edit Profile</h2>
+<p>Foto Profile </p>
+<img src="./img/649190d58e696.jpg" alt="" class="w-32 mx-auto border border-red-500">
 </section>
 @endsection
 @section('footer')
