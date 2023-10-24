13376f6730c02019fd142e55e21323e65dfed919
Rio Saputro <rio.ae23@gmail.com>
2023-10-24 20:28:44 +0700

    change button

--- librarow/resources/views/user/edit_profile.blade.php
+++ librarow/resources/views/user/edit_profile.blade.php
@@ -5,6 +5,7 @@
 <h2 class="font-bold text-lg mt-3 mb-1">Edit Profile</h2>
 <p>Foto Profile </p>
 <img src="./img/649190d58e696.jpg" alt="" class="w-32 mx-auto border border-red-500">
+<input type="file" name="profile_picture" id="" class=" file:bg-blue-500 file:text-white file:rounded-xl py-2">
 </section>
 @endsection
 @section('footer')
