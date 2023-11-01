dbaf87fdf75168d1d6fb9400a56ae0f526650e67
Rio Saputro <rio.ae23@gmail.com>
2023-11-01 23:20:49 +0700

    adding table

--- librarow/resources/views/admin/data_mahasiswa.blade.php
+++ librarow/resources/views/admin/data_mahasiswa.blade.php
@@ -6,6 +6,29 @@
         <input type="search" name="carimhs" id="carimahs" placeholder="Cari Mahasiswa" class="w-64 ms-3 px-2 py-1 focus:outline-blue-500"> 
         <button type="submit" class="bg-white border border-blue-500 rounded-lg px-5 py-1">Cari</button>
        </div>
-       
+       <div class="w-full h-4/6 border border-red-500 text-center">
+            <table class="table-auto w-full border border-blue-300">
+               <thead class="border border-blue-300">
+                <th class="border border-blue-300 w-1/12">No</th>
+                <th class="border border-blue-300 w-2/12">Nama</th>
+                <th class="border border-blue-300 w-2/12">NIM</th>
+                <th class="border border-blue-300 w-2/12">Email</th>
+                <th class="border border-blue-300 w-2/12">Status</th>
+                <th class="border border-blue-300 w-3/12">Action</th>
+               </thead>
+               <tr class="h-10">
+                     <td class="border border-blue-300">1</td>
+                     <td class="border border-blue-300">Rizky</td>
+                     <td class="border border-blue-300">123456789</td>
+                     <td class="border border-blue-300">xxx@gmail.com</td>
+                    <td class="border border-blue-300">Aktif</td>
+                    <td class="border border-blue-300   ">
+                            <button class="bg-blue-500 text-white px-2 py-1 rounded-lg ">Edit</button>
+                            <button class="bg-red-500 text-white px-2 py-1 rounded-lg ">Delete</button>
+               </tr>
+              
+            
+            </table>
+       </div>
     </section>
 @endsection
