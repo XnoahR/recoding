ae80fd0422a4482b36496ae81393c91904ee3878
Rio Saputro <rio.ae23@gmail.com>
2023-11-04 03:22:05 +0700

    Im confused

--- librarow/resources/views/login.blade.php
+++ librarow/resources/views/login.blade.php
@@ -4,15 +4,15 @@
 <body class="bg-gradient-to-br from-cyan-600 to-blue-500 bg-no-repeat">
     <div class="w-full h-screen flex items-center justify-center">
        <div class="w-2/3 h-5/6 bg-white rounded-xl flex ">
-        <div id="login-banner" class="h-full w-1/2 bg-library bg-cover bg-no-repeat bg-center rounded-l-xl flex flex-col justify-end ">
-            {{-- <img src="{{ asset("./img/lib.jpg") }}" alt="" class="w-full h-full object-cover contrast-75 rounded-l-xl"> --}}
+        <div id="login-banner" class="h-full w-1/2 border border-red-500 rounded-l-xl flex flex-col justify-end reg-banner">
+            {{-- <img src="{{ asset("./img/lib.jpg") }}" alt="" class="w-full h-full object-cover contrast-75 rounded-l-xl">
            
             <form action="" id="register-form" method="post" class="reg-form hidden">
                 <input type="email" name="email" id="" class="bg-gray-100 my-3 px-3 w-1/2 h-8 mx-auto border-l-8 border-blue-500">
                 <input type="password" name="password" id="" class="bg-gray-100 my-3 px-3 w-1/2 h-8 mx-auto border-l-8 border-blue-500">
                 <button type="submit" class="bg-blue-500 text-white w-1/2 mx-auto h-8 rounded-md">Login</button>
             </form>
-           <h1 id="create-account" class="text-blue-300 text-xl text-center font-bold mb-5 underline z-50 ">Create an Account!</h1>
+           <h1 id="create-account" class="text-blue-300 text-xl text-center font-bold mb-5 underline z-50 ">Create an Account!</h1> --}}
         </div>
 
         <div id="register-banner" class="h-full w-1/2 flex flex-col justify-center space-y-12  ">
