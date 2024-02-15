2740a2ea26aa1cc2b66c8f5b34ecda3eeec5a788
Rio Saputro <rio.ae23@gmail.com>
2024-02-15 21:35:13 +0700

    Modal Done

--- librarow/resources/views/layouts/modal.blade.php
+++ librarow/resources/views/layouts/modal.blade.php
@@ -10,9 +10,12 @@
     @include('layouts.nav')
 
     <main>
-        <div class="w-full h-screen fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-20">
+       {{-- Modal Button --}}
+         <button class="showModal bg-blue-500 px-3 py-1 rounded text-white showModal">Show Modal</button>
+
+        <div class="modal hidden w-full h-screen fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-20 ">
         {{-- Modal --}}
-        <div class="bg-gray-400 rounded shadow-lg w-1/3">
+        <div class="bg-gray-400 rounded shadow-lg w-1/3 ">
             {{-- Modal Header --}}
             <div class="border-b px-4 py-2">
                 <h3>Modal Title</h3>
@@ -23,10 +26,24 @@
             </div>
             <div class="flex justify-end items-center w-100 border-t p-3">
                 <button class="bg-blue-600 px-3 py-1 rounded text-white">Okay</button>
-                <button class="bg-red-600 px-3 py-1 rounded text-white">Cancel</button>
+                <button class="bg-red-600 px-3 py-1 rounded text-white closeModal">Cancel</button>
             </div>
         </div>
     </div>
+
+    <script>
+        const modal = document.querySelector('.modal')
+        const showModal = document.querySelector('.showModal')
+        const closeModal = document.querySelector('.closeModal')
+
+        showModal.addEventListener('click', () => {
+            modal.classList.remove('hidden')
+        })
+        closeModal.addEventListener('click', () => {
+            modal.classList.add('hidden')
+        })
+    </script>
+
     </main>
 
     @yield('footer')
